6b888149471ca5ec7c04f206ea04bbe01f10462d
Courtney Williams <courtneynrwilliams@gmail.com>
2023-01-16 14:47:01 -0500

    add about and contact pages
    Signed-off-by: Courtney Williams <courtneynrwilliams@gmail.com>

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,10 +8,13 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
+    path('about/', views.about, name='about'),
 
     # path for contact us view
+    path('contact/', views.contact, name='contact')
 
     # path for registration
 
@@ -19,10 +22,9 @@ urlpatterns = [
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
-
     # path for dealer reviews view
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
